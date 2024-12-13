5a6431e9654ceb3d077db92746a7756fa1b799f2
pepedd864 <pepedd@qq.com>
2024-12-13 21:32:47 +0800

    feat: 一些小修小改

--- src/main/webapp/js/apis/exhibits.js
+++ src/main/webapp/js/apis/exhibits.js
@@ -14,3 +14,72 @@ export function createExhibits(contextPath, data) {
         })
     })
 }
+
+// exhibitId
+export function deleteExhibits(contextPath, data) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: contextPath + '/api/exhibits/delete',
+            type: 'delete',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            success: function (response) {
+                resolve(response)
+            },
+            error: function (xhr, status, error) {
+                reject(error)
+            }
+        })
+    })
+}
+
+export function updateExhibits(contextPath, data) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: contextPath + '/api/exhibits/update',
+            type: 'post',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            success: function (response) {
+                resolve(response)
+            },
+            error: function (xhr, status, error) {
+                reject(error)
+            }
+        })
+    })
+}
+
+// export function updateExhibits(contextPath, data) {
+//     return new Promise((resolve, reject) => {
+//         $.ajax({
+//             url: contextPath + '/api/exhibits/update',
+//             type: 'post',
+//             data: JSON.stringify(data),
+//             contentType: 'application/json',
+//             success: function (response) {
+//                 resolve(response)
+//             },
+//             error: function (xhr, status, error) {
+//                 reject(error)
+//             }
+//         })
+//     })
+// }
+
+// categoryId
+export function getByCategoryId(contextPath, data) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: contextPath + `/api/exhibits/get/exList/${data}`,
+            type: 'get',
+            contentType: 'application/json',
+            success: function (response) {
+                resolve(response)
+            },
+            error: function (xhr, status, error) {
+                reject(error)
+            }
+        })
+    })
+}
