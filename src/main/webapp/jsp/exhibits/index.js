c07847d9e7d2e8cc68f5af426630d2963b995c0b
pepedd864 <pepedd@qq.com>
2024-12-19 01:29:09 +0800

    feat(category): 优化分类页面样式和交互
    - 添加 3D 翻转效果，提升用户体验
    - 调整编辑表单样式，优化布局
    -改进按钮点击事件处理逻辑- 统一字体样式，提高可读性

--- src/main/webapp/jsp/exhibits/index.js
+++ src/main/webapp/jsp/exhibits/index.js
@@ -35,11 +35,13 @@ export async function initExhibitsList() {
       exhibit.find('.img').attr('src', contextPath + dataItem.img)
       exhibit.find('.title').text(dataItem.title)
       exhibit.find('.desc').html(dataItem.description)
-      // exhibit.css('backface-visibility','visible')
+      exhibit.css('backface-visibility', 'visible')
       exhibit.css('transform', 'rotateY(180deg')
       exhibit.one('transitionend', function () {
-        // exhibit.css('backface-visibility','hidden')
         exhibit.css('transform', 'rotateY(0deg)');
+        exhibit.one('transitionend', function () {
+          exhibit.css('backface-visibility', 'hidden')
+        })
       });
     }
 
